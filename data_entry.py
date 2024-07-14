c27b478ca9a9ede77b41f027acc772b7b005c3a4
Neil Patel <neilpatel235@gmail.com>
2024-07-14 09:06:50 -0400

    data_entery.py todays date

--- data_entry.py
+++ data_entry.py
@@ -5,11 +5,16 @@ CATEGORIES = {"I": "Income", "E": "Expense"}
 def get_date(prompt, allow_default=False):
 	date_str = input(prompt)
 	if allow_default and not date_str:
+		# this will return todays date.
+		# datetime.today() will print "2024-07-14 09:03:37.899155"
+		# datetime.today().strftime("%d-%m-%Y") will format to "14-07-2024"
 		return datetime.today().strftime("%d-%m-%Y")
 
 	try:
+		# ensures the date is in the correct dd-mm-yyyy format.
 		valid_date = datetime.strptime(date_str, "%d-%m-%Y")
-		return valid_date.strptime("%d-%m-%Y")
+		print(valid_date)
+		return valid_date.strftime("%d-%m-%Y")
 	except ValueError:
 		print("Invalid date frmat. Please enter the date in dd-mm-yyyy format")
 		return get_date(prompt, allow_default)
